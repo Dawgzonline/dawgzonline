e1c050230469024197b3af06a13c80fe3ee6cb3c
nikhjha <nikh.jha550@gmail.com>
2022-01-08 13:13:53 +0530

    Adding boiler next app and configuring it for static uses

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+const withPlugins = require("next-compose-plugins");
+const withOptimizedImages = require("next-optimized-images");
+
+// next.js configuration
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPlugins(
+  [
+    withOptimizedImages({
+      handleImages: ["jpeg", "png", "svg"],
+    }),
+  ],
+  nextConfig
+);
