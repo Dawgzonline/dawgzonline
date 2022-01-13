5b5c71c78072f54bb01c616b795a6ca24dbb539e
nikhjha <nikh.jha550@gmail.com>
2022-01-13 17:24:35 +0530

    Changing info margin and padding

--- frontend/components/TermsAndConditionsPage.js
+++ frontend/components/TermsAndConditionsPage.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import Info from "../Info";
+import React from "react";
+import Info from "./Info";
 
-export default function ContactUsPage() {
+export default function TermsAndConditionsPage() {
   const contents = [
     {
       title: "Address",
       content:
         "B1, Snow White C.H.S., 18th Road, Khar West, Opp. Fab India. Mumbai, Maharashtra 400052",
-      width : {
-        width : "6rem"
-      }
+      width: {
+        width: "6rem",
+      },
     },
     {
       title: "Contact Number",
       content: "+91 9930 924 028   +91 9930 824 029",
-      width : {
-        width : "7rem"
+      width: {
+        width: "9rem",
       },
     },
     {
       title: "Timings",
       content: "10 AM to 8 PM",
-      width : {
-        width : "9rem"
+      width: {
+        width: "9rem",
       },
     },
   ];
 
-    return <Info heading="CONTACT US" contents={contents} />
-
+  return <Info heading="Tearms And Conditions" contents={contents} />;
 }
